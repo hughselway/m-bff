90d25a74736fbd9fec5ad0e6fb972ca79593716a
hughselway <hrs47@cam.ac.uk>
2019-07-19 14:29:57 +0100

    added in support for borderchange microservice

--- bff/game-resource.go
+++ bff/game-resource.go
@@ -2,9 +2,10 @@ package bff
 
 import (
 	"context"
-	pbgameengine "github.com/emailtovamos/m-apis/m-game-engine/v1"
-	pbhighscore "github.com/emailtovamos/m-apis/m-highscore/v1"
 	"github.com/gin-gonic/gin"
+	pbborderchange "github.com/hughselway/m-apis/m-borderchange"
+	pbgameengine "github.com/hughselway/m-apis/m-game-engine/v1"
+	pbhighscore "github.com/hughselway/m-apis/m-highscore/v1"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"strconv"
@@ -13,12 +14,14 @@ import (
 type gameResource struct {
 	gameClient       pbhighscore.GameClient
 	gameEngineClient pbgameengine.GameEngineClient
+	gameBorderClient pbborderchange.GameBorderClient
 }
 
-func NewGameResource(gameClient pbhighscore.GameClient, gameEngineClient pbgameengine.GameEngineClient) *gameResource {
+func NewGameResource(gameClient pbhighscore.GameClient, gameEngineClient pbgameengine.GameEngineClient, gameBorderClient pbborderchange.GameBorderClient) *gameResource {
 	return &gameResource{
 		gameClient:       gameClient,
 		gameEngineClient: gameEngineClient,
+		gameBorderClient: gameBorderClient,
 	}
 }
 
@@ -60,15 +63,37 @@ func NewGrpcGameEngineServiceClient(serverAddr string) (pbgameengine.GameEngineC
 	return client, nil
 }
 
+func NewGrpcGameBorderServiceClient(serverAddr string) (pbborderchange.GameBorderClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal().Msgf("Failed to dial: %v", err)
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-borderchange connection is nil in m-bff")
+	}
+
+	client := pbborderchange.NewGameBorderClient(conn)
+
+	return client, nil
+}
+
 func (gr *gameResource) SetHighScore(c *gin.Context) {
 	highScoreString := c.Param("hs")
 	highScoreFloat64, err := strconv.ParseFloat(highScoreString, 64)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to convert highscore to float")
 	}
-	gr.gameClient.SetHighScore(context.Background(), &pbhighscore.SetHighScoreRequest{
+	_, err = gr.gameClient.SetHighScore(context.Background(), &pbhighscore.SetHighScoreRequest{
 		HighScore: highScoreFloat64,
 	})
+	if err != nil {
+		log.Error().Err(err).Msg("failed to call SetHighScore")
+	}
 }
 
 func (gr *gameResource) GetHighScore(c *gin.Context) {
@@ -106,4 +131,45 @@ func (gr *gameResource) SetScore(c *gin.Context) {
 	if err != nil {
 		log.Error().Err(err).Msg("Error while setting score in m-game-engine")
 	}
+}
+
+func (gr *gameResource) GetBorder(c *gin.Context) {
+	style := c.Param("st")
+	width := c.Param("wd")
+	shapered, err := strconv.ParseFloat(c.Param("scred"), 64)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to convert red colour to float64")
+	}
+	shapegreen, err := strconv.ParseFloat(c.Param("scgreen"), 64)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to convert green colour to float64")
+	}
+	shapeblue, err := strconv.ParseFloat(c.Param("scblue"), 64)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to convert blue colour to float64")
+	}
+
+	getborderresponse, err := gr.gameBorderClient.GetBorder(context.Background(), &pbborderchange.GetBorderRequest{
+		//get stuff from the c context as in other function
+		Style:           style,
+		Width:           width,
+		ShapeColorRed:   shapered,
+		ShapeColorGreen: shapegreen,
+		ShapeColorBlue:  shapeblue,
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("error in changing border from m-borderchange")
+	}
+
+	crString := strconv.FormatFloat(getborderresponse.BorderColorRed, 'e', -1, 64)
+	cgString := strconv.FormatFloat(getborderresponse.BorderColorGreen, 'e', -1, 64)
+	cbString := strconv.FormatFloat(getborderresponse.BorderColorBlue, 'e', -1, 64)
+
+	c.JSONP(200, gin.H{
+		"style":   getborderresponse.Style,
+		"width":   getborderresponse.Width,
+		"scred":   crString,
+		"scgreen": cgString,
+		"scblue":  cbString,
+	})
 }
--- cli/main.go
+++ cli/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"flag"
-	"github.com/emailtovamos/m-bff/bff"
 	"github.com/gin-gonic/gin"
+	"github.com/hughselway/m-bff/bff"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	grpcAddressHighScore := flag.String("address-m-highscore", "localhost:50051", "The grpc server address for highscore service")
 	grpcAddressGameEngine := flag.String("address-m-game-engine", "localhost:60051", "The grpc server address for game-engine service")
+	grpcAddressBorderChange := flag.String("address-m-border-change", "localhost:50052", "The grpc server address for border-change service")
 
 	serverAddress := flag.String("address-http", ":8081", "HTTP server address")
 
@@ -25,13 +26,19 @@ func main() {
 		log.Error().Err(err).Msg("Error in creating a client for m-game-engine")
 	}
 
-	gr := bff.NewGameResource(gameClient, gameEngineClient)
+	gameBorderClient, err := bff.NewGrpcGameBorderServiceClient(*grpcAddressBorderChange)
+	if err != nil {
+		log.Error().Err(err).Msg("Error in creating a client for m-border-change")
+	}
+
+	gr := bff.NewGameResource(gameClient, gameEngineClient, gameBorderClient)
 
 	router := gin.Default()
 	router.GET("/geths", gr.GetHighScore)
 	router.GET("/seths/:hs", gr.SetHighScore)
 	router.GET("/getsize", gr.GetSize)
 	router.GET("/setscore/:score", gr.SetScore)
+	router.GET("/borderchange/st/:st/wd/:wd/scred/:scred/scgreen/:scgreen/scblue/:scblue", gr.GetBorder)
 
 	err = router.Run(*serverAddress)
 	if err != nil {
